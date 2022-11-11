095e0189b5ceb8fe05e9da8bc3d9b22e6e89e944
yisun92 <njusunyi@gmail.com>
2022-11-11 15:43:08 -0500

    ignore emit

---
+++ mytests/ignore_emit.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+contract SimpleAuction {
+    // Events that will be emitted on changes.
+    event HighestBidIncreased(address bidder, uint amount);
+    event AuctionEnded(address winner, uint amount);
+
+    // Errors that describe failures.
+
+    // The triple-slash comments are so-called natspec
+    // comments. They will be shown when the user
+    // is asked to confirm a transaction or
+    // when an error is displayed.
+
+    /// The auction has already ended.
+    error AuctionAlreadyEnded();
+    /// There is already a higher or equal bid.
+    error BidNotHighEnough(uint highestBid);
+    /// The auction has not ended yet.
+    error AuctionNotYetEnded();
+    /// The function auctionEnd has already been called.
+    error AuctionEndAlreadyCalled();
+
+
+    /// Bid on the auction with the value sent
+    /// together with this transaction.
+    /// The value will only be refunded if the
+    /// auction is not won.
+    function bid() external payable {
+        emit HighestBidIncreased(msg.sender, msg.value);
+        emit AuctionEndAlreadyCalled();
+    }
+}
